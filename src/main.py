4378a5aa27308b8ab194ff5a46cb529c1a2aa9c9
Harpartap Singh <91721639+h4rj33@users.noreply.github.com>
2025-11-29 16:15:59 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
 # Main Code
+from azure_ingest import run_azure_ingestion
+from snowflake_ingest import get_source_tables,run_parallel_ingestion
+from slack_utils import slack_etl_summary
+
+if __name__=="__main__":
+    print("\n ETL PIPELINE STARTED\n")
+
+    source_tables=get_source_tables()
+    azure_results=run_azure_ingestion()
+    snowflake_results=run_parallel_ingestion(source_tables,max_workers=6)
+
+    final={**azure_results,**snowflake_results}
+    slack_etl_summary(final)
+
+    print("\n ETL PIPELINE FINISHED\n")
